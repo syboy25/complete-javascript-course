983ba7a2e89c2af17cc9958adebcac827ccdd733
syboy25 <saed.herring25@gmail.com>
2023-11-07 10:06:58 -0500

    Practice with data types

---
+++ 01-Fundamentals-Part-1/starter/datatypes.js
@@ -0,0 +1,15 @@
+let jsIsFun = true; //varaible assasigned boolean true
+let thisBoolean = true; //variable assigned a boolean data type value
+let thisNumber = 25; //variable assigned a number data type value
+let thisString = 'Sy'; //variable assigned a string data type value
+let thisUndefined; //undefined data type value
+
+console.log(`jsIsFun value is: ${thisBoolean}`); //will return true
+console.log(`The type of value jsIsFun is: ${typeof jsIsFun}`); 
+// console.log(typeof thisBoolean); //will return 'boolean'
+// console.log(typeof thisNumber); //will return 'number'
+// console.log(typeof thisString); //will return 'string'
+// console.log(typeof thisUndefined); //will return 'undefinded'
+
+jsIsFun = 'New Value Assigned!' //assigned a new value to jsIsFun  
+console.log(`jsIsFun new value is: ${jsIsFun}`)
---
+++ 01-Fundamentals-Part-1/starter/values_variables.js
@@ -0,0 +1,21 @@
+let js = 'amazing'; 
+if (js === 'amazing') alert('JS is FUN!'); 
+console.log(40+8+23-10);
+
+console.log("Jonas"); //Value is Jonas
+console.log(23); //Value is 23
+
+let firstName = "Sy"; //assassigned variable firstName to the value of Jonas
+ if (firstName === "Sy")
+ {
+    console.log("Name is correct!")
+ }
+console.log(firstName) //Return the value assigned to firstName
+
+let myFirstJob = "Civil Engineer";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
